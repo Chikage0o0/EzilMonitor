00e6462d8d59ccacc9fc33927a072b2baa800c89
Chikage0o0 <chikage@939.me>
2022-02-15 17:33:19 +0800

    推荐使用外界监控控制

--- main.py
+++ main.py
@@ -24,26 +24,24 @@ def sendMessage(i):
         print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+" 无法通知api")
 
 def main():
-    for o in range(60):
-        try:
-            f = open("i.txt","r")
-            i=int(f.read())
-            f.close
-        except:
-            i=0
-        ii=get()
-        print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+" 在线"+str(ii)+"台机器")
-        if ii>i:
-            with open("i.txt",'w') as f:
-                f.write(str(ii))
-        if (ii<i):
-            if ii==-1:
-                sendMessage("无法访问Ezil api")
-            else:
-                sendMessage("掉线了"+str(i-ii)+"台机器")
-            with open("i.txt",'w') as f:
-                f.write(str(ii))
-        time.sleep(60)
+    try:
+        f = open("i.txt","r")
+        i=int(f.read())
+        f.close
+    except:
+        i=0
+    ii=get()
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+" 在线"+str(ii)+"台机器")
+    if ii>i:
+        with open("i.txt",'w') as f:
+            f.write(str(ii))
+    if (ii<i):
+        if ii==-1:
+            sendMessage("无法访问Ezil api")
+        else:
+            sendMessage("掉线了"+str(i-ii)+"台机器")
+        with open("i.txt",'w') as f:
+            f.write(str(ii))
 
 if __name__ == '__main__':
     address = os.environ["ADDRESS"]
